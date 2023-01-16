5927734952f55ada8881dba30fe985a42d0c3828
Sarah Abed Elrazeq <sarah.abed.elrazeq@gmail.com>
2023-01-16 13:53:59 +0200

    done

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,9 +5,5 @@ import { useNavigate } from "react-router-dom";
 export default function Home() {
   const navigate = useNavigate();
 
-  React.useEffect(() => {
-    navigate("/collections/spectacles-women");
-  }, [navigate]);
-
   return <Main id="home-page"></Main>;
 }
